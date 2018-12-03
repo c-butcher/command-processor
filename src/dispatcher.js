d0105a9bd014b12981b47c1a6e4f6599432f1ff0
Chris Butcher <c.butcher@hotmail.com>
2018-12-02 17:03:29 -0800

    Made dispatcher stateful.

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,16 +1,76 @@
 class Dispatcher {
-    constructor() {
-        this._processing = false;
+    /**
+     * Dispatchers are used to help expose crucial parts of the application layer
+     * to our commands, such as the database and template services.
+     * @param options
+     */
+    constructor(options = {}) {
+        this._options = options;
+        this.reset();
     }
 
+    /**
+     * Tells whether the dispatcher can hold its state.
+     *
+     * @returns {boolean|*}
+     */
+    isStateful() {
+        return this._stateful;
+    }
+
+    /**
+     * Sets whether the dispatcher should hold its state.
+     *
+     * @param {boolean} isStateful
+     *
+     * @returns {Dispatcher}
+     */
+    setStateful(isStateful) {
+        this._stateful = isStateful;
+
+        return this;
+    }
+
+    /**
+     * Reset the dispatcher to the default options it was instantiated with,
+     * and also clears out any stateful variables.
+     *
+     * @returns {Dispatcher}
+     */
+    reset() {
+        this._processing = false;
+        this._stateful   = !!this._options.stateful;
+
+        return this;
+    }
+
+    /**
+     * Stop processing commands.
+     *
+     * @returns {Dispatcher}
+     */
     startProcessing() {
         this._processing = true;
+
+        return this;
     }
 
+    /**
+     * Stop processing commands.
+     *
+     * @returns {Dispatcher}
+     */
     stopProcessing() {
         this._processing = false;
+
+        return this;
     }
 
+    /**
+     * Tells whether the dispatcher is current processing commands.
+     *
+     * @returns {boolean}
+     */
     isProcessing() {
         return this._processing;
     }
--- test/dispatcher.js
+++ test/dispatcher.js
@@ -4,14 +4,19 @@ const Dispatcher = require('../src/dispatcher');
 describe('Dispatcher', function() {
 
     describe('constructor()', function() {
-        it('passes when dispatcher does not start traversing immediately', function() {
+        it('passes when dispatcher is not processing', function() {
             let dispatcher = new Dispatcher();
             chai.assert.isFalse(dispatcher.isProcessing());
+        });
+
+        it('passes when dispatcher is not stateful', function() {
+            let dispatcher = new Dispatcher();
+            chai.assert.isFalse(dispatcher.isStateful());
         });
     });
 
     describe('isProcessing()', function() {
-        it('passes when the dispatcher is traversing', function() {
+        it('passes when the dispatcher is processing', function() {
             let dispatcher = new Dispatcher();
             chai.assert.isFalse(dispatcher.isProcessing());
 
@@ -19,7 +24,7 @@ describe('Dispatcher', function() {
             chai.assert.isTrue(dispatcher.isProcessing());
         });
 
-        it('fails when the dispatcher is not traversing', function() {
+        it('fails when the dispatcher is not processing', function() {
             let dispatcher = new Dispatcher();
 
             dispatcher.startProcessing();
@@ -29,4 +34,47 @@ describe('Dispatcher', function() {
             chai.assert.isFalse(dispatcher.isProcessing());
         });
     });
+
+    describe('isStateful()', function() {
+        it('passes when the dispatcher is stateful', function() {
+            let dispatcher = new Dispatcher({ stateful: true });
+            chai.assert.isTrue(dispatcher.isStateful());
+
+        });
+
+        it('fails when the dispatcher is not stateful', function() {
+            let dispatcher = new Dispatcher();
+            chai.assert.isFalse(dispatcher.isStateful());
+        });
+    });
+
+    describe('setStateful()', function() {
+        it('passes when you can change the state', function() {
+            let dispatcher = new Dispatcher();
+            chai.assert.isFalse(dispatcher.isStateful());
+
+            dispatcher.setStateful(true);
+            chai.assert.isTrue(dispatcher.isStateful());
+
+        });
+    });
+
+    describe('reset()', function() {
+        it('passes when options get reset to original options.', function() {
+            // Starts as stateless and not processing
+            let dispatcher = new Dispatcher();
+            chai.assert.isFalse(dispatcher.isStateful());
+            chai.assert.isFalse(dispatcher.isProcessing());
+
+            // Sets the dispatcher to stateful and processing
+            dispatcher.setStateful(true).startProcessing();
+            chai.assert.isTrue(dispatcher.isStateful());
+            chai.assert.isTrue(dispatcher.isProcessing());
+
+            // Reset the dispatcher back to the original options
+            dispatcher.reset();
+            chai.assert.isFalse(dispatcher.isStateful());
+            chai.assert.isFalse(dispatcher.isProcessing());
+        });
+    })
 });
