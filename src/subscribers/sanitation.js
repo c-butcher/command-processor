208f00675be609ac35e2e242ae3e5a927bbfb2af
Chris Butcher <c.butcher@hotmail.com>
2018-12-07 21:38:40 -0800

    Cleaned up the sanitation and validation subscribers so they are more debuggable and don't pollute the global namespace.

--- src/subscribers/sanitation.js
+++ src/subscribers/sanitation.js
@@ -1,17 +1,25 @@
 const Sanitizer = require('data-sanitizers').Sanitizer;
 const Events = require('../events');
 
-/**
- * @var {InputSanitationEvent} event
- */
-Events.on(Events.INPUT_SANITATION, /** @param {InputSanitationEvent} event */ (event) => {
-    let options = event.getOptions();
-    let type    = event.getType();
-    let value   = event.getValue();
+((Events, Sanitizer) => {
+    /**
+     * Checks to see if we have a sanitizer for the input type, and if so
+     * then we execute the sanitizer and return the sanitized value.
+     *
+     * @param {InputSanitationEvent} event
+     */
+    function sanitationEventHandler(event) {
+        let type    = event.getType();
+        let value   = event.getValue();
+        let options = event.getOptions();
 
-    if (Sanitizer.has(type)) {
-        value = Sanitizer.clean(value, type, options);
+        if (Sanitizer.has(type)) {
+            value = Sanitizer.clean(value, type, options);
+        }
+
+        event.setSanitized(value);
     }
 
-    event.setSanitized(value);
-});
+    Events.on(Events.INPUT_SANITATION, sanitationEventHandler);
+})(Events, Sanitizer);
+
--- src/subscribers/validation.js
+++ src/subscribers/validation.js
@@ -1,15 +1,27 @@
 const Validator = require('data-validators').Validator;
 const Events = require('../events');
 
-/**
- * @var {InputEvent} event
- */
-Events.on(Events.INPUT_VALIDATION, /** @param {InputValidationEvent} event */ (event) => {
-    let type    = event.getType();
-    let value   = event.getValue();
-    let options = event.getOptions();
+((Events, Validator) => {
+    /**
+     * Check to see if we have a validator to handle the input type, and if so
+     * then we execute the validation check and add any errors to our event.
+     *
+     * @param {InputValidationEvent} event
+     */
+    function validationEventHandler(event) {
+        let type = event.getType();
 
-    if (Validator.has(type)) {
-        Validator.check(value, type, options).forEach(event.addError);
+        if (Validator.has(type)) {
+            let value   = event.getValue();
+            let options = event.getOptions();
+            let errors  = Validator.check(value, type, options);
+
+            if (errors.length > 0) {
+                errors.forEach(event.addError)
+            }
+        }
     }
-});
+
+    Events.on(Events.INPUT_VALIDATION, validationEventHandler);
+})(Events, Validator);
+
