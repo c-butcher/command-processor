c79e5cecb3a80766087356ecd90cceb61459a986
Chris Butcher <c.butcher@hotmail.com>
2018-12-09 14:24:40 -0800

    Added input builder and process builder to main library loader.

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const CommandEvent = require('./src/events/command-event');
 const Dispatcher = require('./src/dispatcher');
 const Events = require('./src/events');
 const Input = require('./src/input');
+const InputBuilder = require('./src/builders/input-builder');
 const InputSanitationEvent = require('./src/events/input-sanitation-event');
 const InputValidationEvent = require('./src/events/input-validation-event');
 const Process = require('./src/process');
+const ProcessBuilder = require('./src/builders/process-builder');
 
 let softDependencies = {
     "data-sanitizers": "./src/subscribers/sanitation",
@@ -24,7 +26,9 @@ module.exports = {
     Dispatcher,
     Events,
     Input,
+    InputBuilder,
     InputSanitationEvent,
     InputValidationEvent,
     Process,
+    ProcessBuilder
 };
